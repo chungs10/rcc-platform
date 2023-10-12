60bf071bd092357c8b80b450e499e420d721aadc
Ben Manicke <manicb@rpi.edu>
2023-10-12 09:48:26 -0400

    Modify Calendar Stuffs

--- bid1/src/components/Calendar.js
+++ bid1/src/components/Calendar.js
@@ -1,128 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import '../App.css';
-import axios from 'axios';
 import Fullcalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
-import { saveAs } from 'file-saver';
-
-const convertMonthToDigit = monthName => {
-  const months = {
-    January: '01',
-    February: '02',
-    March: '03',
-    April: '04',
-    May: '05',
-    June: '06',
-    July: '07',
-    August: '08',
-    September: '09',
-    October: '10',
-    November: '11',
-    December: '12'
-  };
-
-  return months[monthName];
-};
-
-// Function to customize event content
-const renderEventContent = ({ event }) => {
-  return (
-    <>
-      <b>{event.title}</b>
-      <br />
-      <span style={{ fontSize: '12px' }}>
-        {event.start.toLocaleString()}
-      </span>
-      <br />
-      <a href={event.url} target="_blank" rel="noopener noreferrer">View Details</a>
-    </>
-  );
-};
-
-
-
-
-
+import "./Calendar.css"
 
 function Calendar() {
-  const [events, setEvents] = useState([]);
 
-  useEffect(() => {
-    fetchEvents();
-  }, []);
-
-  const fetchEvents = () => {
-    axios
-      .get(`${(window.location.href.includes('localhost')) ? 'http://localhost:3000' : 'https://rpi-composite-server-a93993c40133.herokuapp.com'}/acalRaw`)
-      .then(response => {
-        const eventData = response.data;
-
-        const parsedEvents = [];
-
-        for (const monthYear in eventData) {
-          if (eventData.hasOwnProperty(monthYear)) {
-            const eventsForMonth = eventData[monthYear];
-
-            for (const date in eventsForMonth) {
-              if (eventsForMonth.hasOwnProperty(date)) {
-                const event = eventsForMonth[date];
-
-                console.log(date);
-
-                let [monthName, day, year] = date.split(' ');
-
-                day = day.slice(0,-1);
-
-                const month = convertMonthToDigit(monthName);
-
-                const formattedDate = `${year}-${month}-${day}`;
-
-                const eventObj = {
-                  title: event.txt,
-                  start: formattedDate,
-                  url: event.href,
-                  // Add other properties like 'end' if applicable
-                  // end: '...',
-                  // ...
-                };
-
-                parsedEvents.push(eventObj);
-              }
-            }
-          }
-        }
-
-        setEvents(parsedEvents);
-      })
-      .catch(error => {
-        console.error(error);
-      });
+  const dayHeaderContent = (info) => {
+    // Add a custom class to day header elements
+    return (<div className="custom-day-header">${info.dayNumberText}</div>);
   };
 
-  const handleExportICS = () => {
-    fetch(`${(window.location.href.includes('localhost')) ? 'http://localhost:3000' : 'https://rpi-composite-server-a93993c40133.herokuapp.com'}/calics`)
-      .then(response => response.blob())
-      .then(blob => {
-        saveAs(blob, 'calendar.ics');
-      })
-      .catch(error => {
-        console.error('Error fetching ICS file:', error);
-      });
-  };
-  
   return (
-    <div>
+    <div className='calendarHolder'>
       <Fullcalendar
         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
         initialView={"dayGridMonth"}
-        eventContent={renderEventContent} // Call renderEventContent function for custom event rendering
-        events={events}
         customButtons={{
           exportICS: {
             text: "Export ICS",
-            click: handleExportICS,
           },
         }}
         headerToolbar={{
@@ -133,7 +31,8 @@ function Calendar() {
         footerToolbar={{
           end: "exportICS",
         }}
-        height={"90vh"}
+        height={"82vh"}
+        
       />
     </div>
   );
