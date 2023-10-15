4d89d0c7901665f0d60fabcf70f4db37df5f1260
Ben Manicke <manicb@rpi.edu>
2023-10-15 14:21:34 -0400

    Weird Syncing Issue, Attempted Fix #1
    I tried using a Bootstrap template a WHILE ago. For some reason, it just merged with this current project weeks later. Getting rid of all the clutter right now...

---
+++ bid1/src/components/Donation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+import './Donation.css'
+
+function Donation() {
+  return (
+    <div className='donationContainer'>
+        
+    </div>
+  );
+}
+
+export default Donation;
--- bid1/src/pages/Support.js
+++ bid1/src/pages/Support.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Donation from '../components/Donation.js'
 import '../App.css';
 
 function Support() {
   return (
     <>
         <h1>How to Support</h1>
+        <Donation />
     </>
   );
 }
