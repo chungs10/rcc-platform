daf380330fe67dc7d3bf4a47ce52dfcbf37cef81
Peter-krumpholz <91489380+Peter-krumpholz@users.noreply.github.com>
2023-11-27 19:20:18 -0500

    Update DB_interaction.js

--- bid2/DB_interaction.js
+++ bid2/DB_interaction.js
@@ -11,78 +11,96 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB Connection
-mongoose.connect('mongodb+srv://krumpp:M1TRRCC@rcc-cluster.h5eohrh.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
+(async () => {
+  try {
+    await mongoose.connect('mongodb+srv://krumpp:M1TRRCC@rcc-cluster.h5eohrh.mongodb.net/');
+
     console.log('Connected to MongoDB');
-});
 
-// Define a MongoDB schema
-const itemSchema = new mongoose.Schema({
-    CTF: [
-        {
-            title: String,
-            problem: String,
-            Connect: String,
-            solution: String,
-            img: [
-                {
-                    name: String,
-                    img: String,
-                },
-            ],
-            links: [
-                {
-                    link: String,
-                },
-            ],
-            answer: String,
+    // Define a MongoDB schema
+    const itemSchema = new mongoose.Schema({
+        CTF: {
+          title: String,
+          problem: String,
+          Connect: String,
+          solution: String,
+          links: [
+            {
+              link: String,
+            },
+          ],
+          answer: String,
         },
-    ],
-});
+      });
+  
+      // Model for the Bid-2 collection
+      const Bid2 = mongoose.model('Bid-2', itemSchema);
 
-const Item = mongoose.model('Item', itemSchema);
 
-// Routes
-app.post('/api/items', async (req, res) => {
-    try {
-        const newItem = new Item(req.body);
-        const savedItem = await newItem.save();
-        res.json(savedItem);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
-app.get('/api/items', async (req, res) => {
-    try {
-        const items = await Item.find();
-        res.json(items);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
-app.put('/api/items/:id', async (req, res) => {
-    try {
-        const updatedItem = await Item.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    // Routes
+    app.post('/api/items', async (req, res) => {
+        try {
+          const newItem = new Bid2(req.body);  
+          const savedItem = await newItem.save();
+          res.json(savedItem);
+        } catch (error) {
+          res.status(500).json({ error: error.message });
+        }
+      });
+      
+      app.get('/api/items/:title', async (req, res) => {
+        const title = req.params.title;
+    
+        try {
+            const query = {};
+            query[`CTF.${title}`] = { $exists: true };
+            const challenge = await Bid2.findOne(query);
+    
+            console.log('Title:', title);
+            console.log('MongoDB Query Result:', challenge);
+    
+            if (!challenge) {
+                return res.status(404).json({ message: 'Item not found' });
+            }
+    
+            res.json(challenge.CTF);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Internal server error' });
+        }
+    });
+    
+      
+      
+      
+    app.put('/api/items/:id', async (req, res) => {
+      try {
+        const updatedItem = await Bid2.findByIdAndUpdate(req.params.id, req.body, { new: true });
         res.json(updatedItem);
-    } catch (error) {
+      } catch (error) {
         res.status(500).json({ error: error.message });
-    }
-});
+      }
+    });
 
-app.delete('/api/items/:id', async (req, res) => {
-    try {
-        const deletedItem = await Item.findByIdAndRemove(req.params.id);
+    app.delete('/api/items/:id', async (req, res) => {
+      try {
+        const deletedItem = await Bid2.findByIdAndRemove(req.params.id);
         res.json(deletedItem);
-    } catch (error) {
+      } catch (error) {
         res.status(500).json({ error: error.message });
-    }
-});
+      }
+    });
 
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+    // Default route handler for the root path
+    app.get('/', (req, res) => {
+        res.send('Server is running. Use /api/items to interact with the API.');
+      });
+  
+      // Start the server
+      app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+      });
+    } catch (error) {
+      console.error('Error connecting to MongoDB:', error.message);
+    }
+})();
